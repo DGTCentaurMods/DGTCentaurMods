3c0d06ca9dec7616453e8b08e0abe518aa9bc015
Adrian <wormstein@gmail.com>
2021-10-01 19:48:02 +0100

    Add script to deploy python environment and install packages.

---
+++ dgt_centaur_mods/scripts/deploy-python-env.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+# Check if pip is installed
+if sudo dpkg -l | grep -q python-pip
+then
+    echo "::: Pip is installed."
+else
+    echo "::: Pip not installed. Installing now..."
+    sudo apt-get install -y python-pip
+fi
+
+# Check if virtualenv is installe
+if sudo pip3 list | grep virtualenv
+then
+    echo "::: Virtualenv is installed."
+else
+   echo "::: Virtualenv missing, Installing..."
+   sudo pip3 inst5all virtualenv
+fi
+
+# Deploy python environment
+cd ../..
+echo "::: Deploying python env..."
+virtualenv .venv
+
+echo "::: Installing packages inside the environment..."
+./.venv/bin/pip3 install -r requirements.txt
+
+echo "::: All done."
+echo ":: Activate environment using \"source .venv/bin/activate\""
+
