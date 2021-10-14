c0ae9c6a82f708a6f61aa8df35fa1914fc591583
Ed Nekebno <ednekebno@gmail.com>
2021-10-14 21:34:06 +0100

    Ajax fen fetch on web

--- DGTCentaurMods/web/app.py
+++ DGTCentaurMods/web/app.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
 from board import LiveBoard
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    fenlog = "/home/pi/centaur/fen.log"
-    with open(fenlog, "r") as f:
-        line = f.readline().split(' ')
-        fen = line[0]
-    return render_template('index.html', fen=fen)
+	fenlog = "/home/pi/centaur/fen.log"
+	if os.path.isfile(fenlog) == True:
+		with open(fenlog, "r") as f:
+			line = f.readline().split(" ")
+			fen = line[0]
+	else:
+		fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	return render_template('index.html', fen=fen)
 
-
+@app.route("/fen")
+def fen():
+	fenlog = "/home/pi/centaur/fen.log"
+	if os.path.isfile(fenlog) == True:
+		with open(fenlog, "r") as f:
+			line = f.readline().split(" ")
+			fen = line[0]
+	else:
+		fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	return fen
