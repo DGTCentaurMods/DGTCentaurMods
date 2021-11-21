946f196e8f94b62d0a21af35b2c0cf7bd5b7011a
Ed Nekebno <ednekebno@gmail.com>
2021-11-21 17:07:41 +0000

    fix temp files

--- DGTCentaurMods/config/wifi.py
+++ DGTCentaurMods/config/wifi.py
@@ -1,5 +1,25 @@
 # Connect to a Wifi Network
 #
+# This file is part of the DGTCentaur Mods open source software
+# ( https://github.com/EdNekebno/DGTCentaur )
+#
+# DGTCentaur Mods is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# DGTCentaur Mods is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see
+#
+# https://github.com/EdNekebno/DGTCentaur/blob/master/LICENSE.md
+#
+# This and any other notices must remain intact and unaltered in any
+# distribution, modification, variant, or derivative of this software.
 
 from DGTCentaurMods.board import board
 import os
@@ -37,35 +57,4 @@ if answer == "BACK":
 board.epd.init()
 
 # If the answer is not "BACK" then answer contains our SSID
-# Now we need to get the password
-password = board.getText("Wifi Password")
-
-if password == "":
-	sys.exit()
-
-# Add an SSID
-cmd = """sudo sh -c \"wpa_passphrase '""" + answer + """' '""" + password + """'\""""
-res = os.popen(cmd)
-result = list(res)
-section = ""
-for i in range(0,len(result)):
-	section = section + result[i]
-print(section)
-if section.find("ssid") != -1:
-	wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','r')
-	curconf = wpas.read()
-	wpas.close()
-	print(curconf)
-	if curconf.find(answer) != -1:
-		# SSID is already in file
-		newtext = re.sub('network={[^\}]+?ssid=\"' + answer + '\"[^\}]+?\}\n','',curconf,re.DOTALL)
-		print(newtext)
-		wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','w')
-		wpas.write(newtext)
-		wpas.close()
-	# Success - append it to wpa_supplicant
-	wpas = open('/etc/wpa_supplicant/wpa_supplicant.conf','a')
-	wpas.write(section)
-	wpas.close()
-	os.system("sudo wpa_cli -i wlan0 reconfigure")
-
+# Now we n
