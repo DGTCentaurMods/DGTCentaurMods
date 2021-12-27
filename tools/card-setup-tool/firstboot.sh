3df3ca79f95a50ab08715a546b62767100422e9f
marcb1309 <74376513+marcb1309@users.noreply.github.com>
2021-12-27 19:08:12 +0100

    add logging to the install scripts

--- tools/card-setup-tool/firstboot.sh
+++ tools/card-setup-tool/firstboot.sh
@@ -1,10 +1,20 @@
 #!/bin/bash 
-apt get update
-apt get -y upgrade
-apt get -y full-upgrade
+echo $(date +"%T") "apt get update" >>/boot/centaur_install.log
+apt update
+echo $(date +"%T")  "apt get upgrade" >>/boot/centaur_install.log
+apt -y upgrade
+echo $(date +"%T")  "apt get full-upgrade" >>/boot/centaur_install.log
+apt -y full-upgrade
+echo $(date +"%T")  "apt -y install /boot/DGTCentaurMods" >>/boot/centaur_install.log
 apt -y install /boot/DGTCentaurMods_0-build_armhf.deb
-
+echo $(date +"%T")  "disable firstboot.service" >>/boot/centaur_install.log
 systemctl disable firstboot.service 
+echo $(date +"%T")  "remove /etc/systemd/system/firstboot.service" >>/boot/centaur_install.log
 rm -rf /etc/systemd/system/firstboot.service
+echo $(date +"%T")  "remove /home/pi/firstboot.sh" >>/boot/centaur_install.log
 rm -f /home/pi/firstboot.sh
+echo $(date +"%T")  "reboot 2" >>/boot/centaur_install.log
 reboot
+
+
+
