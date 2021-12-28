417637bcd874fc518a1643bad2056953036f5dc7
Adrian <wormstein@gmail.com>
2021-12-28 20:51:52 +0000

    Add license header.

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/bash
-# Script to produce deb pacjage
+#
+#This is a tool to build deb packages.
+#
+#This file is part of the DGTCentaur Mods open source software
+# ( https://github.com/EdNekebno/DGTCentaur )
+#
+# DGTCentaur Mods is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# DGTCentaur Mods is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see
+#
+# https://github.com/EdNekebno/DGTCentaur/blob/master/LICENSE.md
+#
+# This and any other notices must remain intact and unaltered in any
+# distribution, modification, variant, or derivative of this software.
+
 
 ##### VARIABLES
 BASE=`pwd`
