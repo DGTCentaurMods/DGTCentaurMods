1fcaa8266d3a191e0d3ac2d868041f767beb6cd9
Adrian <wormstein@gmail.com>
2022-01-28 19:00:42 +0000

    Enable switching of engines folder.

--- tools/dev-tools/switch-env.sh
+++ tools/dev-tools/switch-env.sh
@@ -14,29 +14,52 @@ function restartServices {
 }
 
 
+function moveEnginesFolder {
+    if [ -d ${INSTALL_DIR}/.${PCK_NAME}/engines ]; then
+        read -p "Do you want to link engines too? (y/n): "
+        case $REPLY in
+            [Yy]* ) 
+                echo -e "--> Linking engines folder:
+                ${INSTALL_DIR}/.${PCK_NAME}/engines --> ${PCK_NAME}/engines"
+                mv ${BASE}/${PCK_NAME}/engines ${BASE}/${PCK_NAME}/.engines
+                ln -s ${INSTALL_DIR}/.${PCK_NAME}/engines ${BASE}/${PCK_NAME}/engines
+                ;;
+            [Nn]* ) break ;; 
+            * ) break ;;
+        esac
+    fi
+}
+
+
 function toggle {
     ### Check if we are already switched
-    if [ -e ${BASE}/.toggled ]
-    then
-        cd ${INSTALL_DIR}
+    if [ -e ${BASE}/.toggled ]; then
         echo -e "This repository is live on board in ${INSTALL_DIR}."
         echo -e "Deactivating."
-            rm ${PCK_NAME}
-            if [ -d .${PCK_NAME} ]
-            then
-                mv .${PCK_NAME} ${PCK_NAME} && rm $BASE/.toggled
-            fi
-            restartServices
-        echo -e "Done. Have a nice day!"
+        # Checki if engines are linked.
+        cd ${BASE}/${PCK_NAME}
+        if [ -d .engines ]; then
+            echo -e "-- > Unlinking engines folder..."
+            rm engines && mv .engines engines
+        fi
+        cd ${INSTALL_DIR}
+        rm ${PCK_NAME}
+        if [ -d .${PCK_NAME} ]; then
+            mv .${PCK_NAME} ${PCK_NAME} && rm $BASE/.toggled
+        fi
+        restartServices
+    echo -e "Done. Have a nice day!"
     else
         echo -e "Installing in ${INSTALL_DIR}"
         cd ${INSTALL_DIR}
-        if [ -d ${PCK_NAME} ]
-        then
+        if [ -d ${PCK_NAME} ]; then
             mv ${PCK_NAME} .${PCK_NAME}
         fi
         ln -s ${BASE}/${PCK_NAME} && touch ${BASE}/.toggled
+        moveEnginesFolder
         restartServices
+        echo -e "!!! You are using now a repo as a QA tier. Be careful on your git commits. !!!"
+        echo -e "Do not add unnecessary files in your commit."
         echo -e "Done. Bye."
     fi
 }
@@ -44,8 +67,7 @@ function toggle {
 
 ## Main
 
-if [ -e $BASE/.toggled ]
-then
+if [ -e $BASE/.toggled ]; then
     echo -e "Your repository is currently installed in ${INSTALL_DIR}"
 else
    echo -e "This repository is not installed."
