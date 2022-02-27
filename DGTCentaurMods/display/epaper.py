49289f4e7ac0ea294c29fb6d10b716a471341e11
Adrian <wormstein@gmail.com>
2022-02-27 18:49:28 +0000

    Fit better the board with the black rectangle

--- DGTCentaurMods/display/epaper.py
+++ DGTCentaurMods/display/epaper.py
@@ -253,9 +253,7 @@ def drawBoard(pieces, startrow=2):
             px = 192
         piece = chessfont.crop((px, py, px+16, py+16))
         epaperbuffer.paste(piece,(col, row))
-    draw.rectangle([(0,48),(128,177)],fill=None,outline='black')
-    #draw.line((0, 47, 128,47), fill=0, width=1)
-    #draw.line((0, 177, 128,177), fill=0, width=1)
+    draw.rectangle([(0,47),(128,176)],fill=None,outline='black')
 
 def drawFen(fen, startrow=2):
     # As drawboard but draws a fen
