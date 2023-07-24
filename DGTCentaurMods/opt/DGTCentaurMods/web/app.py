d7422b620e18f751d478b92a439f867abde1232b
Alistair Crompton <alistair.crompton@moult.org>
2023-07-24 11:42:15 +0200

    Added web events log menu.

--- DGTCentaurMods/opt/DGTCentaurMods/web/app.py
+++ DGTCentaurMods/opt/DGTCentaurMods/web/app.py
@@ -22,14 +22,51 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from DGTCentaurMods.game.lib import common
+from DGTCentaurMods.game.consts import consts
 
 import logging
+
+
+def tail(f, lines=1, _buffer=4098):
+    """Tail a file and get X lines from the end"""
+    # place holder for the lines found
+    lines_found = []
+
+    # block counter will be multiplied by buffer
+    # to get the block size from the end
+    block_counter = -1
+
+    # loop until we find X lines
+    while len(lines_found) < lines:
+        try:
+            f.seek(block_counter * _buffer, os.SEEK_END)
+        except IOError:  # either file is too small, or too many lines requested
+            f.seek(0)
+            lines_found = f.readlines()
+            break
+
+        lines_found = f.readlines()
+
+        # we found enough lines, get out
+        # Removed this line because it was redundant the while will catch
+        # it, I left it for history
+        # if len(lines_found) > lines:
+        #    break
+
+        # decrement the block counter to get the
+        # next X bytes
+        block_counter -= 1
+
+    return lines_found[-lines:]
+
 
 logging.getLogger('chess.engine').setLevel(logging.CRITICAL)
 logging.getLogger("requests").setLevel(logging.CRITICAL)
 logging.getLogger("urllib3").setLevel(logging.CRITICAL)
 logging.getLogger("werkzeug").setLevel(logging.CRITICAL)
 logging.getLogger("urllib3").propagate = False
+
+WEB_NAME = "DGTCentaurMods web 2.0"
 
 appFlask = Flask(__name__)
 
@@ -82,6 +119,10 @@ def on_request(message):
 			time.sleep(.5)
 			os.system("sudo systemctl restart DGTCentaurMods.service")
 
+		if action == "log_events":
+			message = {"log_data":tail(open(consts.LOG_FILENAME, "r"), 100)}
+			socketio.emit('message', message)
+
 	else:
 
 		# Broadcast to all connected clients
@@ -89,7 +130,7 @@ def on_request(message):
 
 @appFlask.route("/")
 def index():
-	return render_template('2.0/index.html', data={"boardsize": 550, "iconsize": int(550/9)})
+	return render_template('2.0/index.html', data={"title":WEB_NAME, "boardsize": 550, "iconsize": int(550/9)})
 
 
 # LEGACY WEB SITE - reachable thru http://localhost/legacy
--- DGTCentaurMods/opt/DGTCentaurMods/web/static/2.0/js/index.js
+++ DGTCentaurMods/opt/DGTCentaurMods/web/static/2.0/js/index.js
@@ -33,65 +33,13 @@ angular.module("dgt-centaur-mods", ['ngMaterial', 'angular-storage', 'ngAnimate'
 		const DEFAULT_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
 		const stockfish = new Worker('static/stockfish/stockfish.js')
+
+		stockfish._lasteval = 0
 		
+		// FEN history (built from PGN)
 		const history = {
 			fens:[],
 			index:0
-		}
-
-		stockfish._lasteval = 0
-
-		stockfish.onmessage = (event) => {
-
-			if (event.data) {
-
-				//console.log(event.data)
-
-				// Stockfish evaluation finishes with the bestmove message
-				if (event.data.includes("bestmove")) {
-					me.board.eval = 50+stockfish._lasteval
-					$scope.$apply()
-				}
-				
-				// Stockfish evaluation feedback
-				if (event.data.includes("score cp")) {
-
-					// info depth 1 seldepth 1 multipv 1 score cp -537 nodes
-					const regexp = /score\ cp\ ([^ ]+)\ /gi
-					const matches = regexp.exec(event.data)
-
-					const MAX_VALUE = 600
-
-					if (matches && matches.length) {
-
-						let value = parseInt(matches[1])
-
-						// black plays?
-						if (me.board.turn == 0) value = -value
-			
-						if (value >  MAX_VALUE) value =  MAX_VALUE
-						if (value < -MAX_VALUE) value = -MAX_VALUE
-
-						value = parseInt((value/(MAX_VALUE *2)) * 100)
-
-						stockfish._lasteval = value
-					}
-				}
-
-				// Stockfish detected (future) mat state
-				if (event.data.includes("score mate")) {
-
-					let value = 50
-
-					// black plays?
-					if (me.board.turn == 0) value = -value
-
-					stockfish._lasteval = value
-
-					me.board.eval = 50+value
-					$scope.$apply()
-				}
-			}
 		}
 
 		// Board data
@@ -102,6 +50,7 @@ angular.module("dgt-centaur-mods", ['ngMaterial', 'angular-storage', 'ngAnimate'
 			eval:50,
 		}
 
+		// Each display menu item is connected to a boolean main.board property
 		const displaySettings = [
 			{ id:"previous_move", default:true,  label: "Previous move", type:"checkbox"},
 			{ id:"kings_checks", default:false,  label: "Kings checks", type:"checkbox"},
@@ -125,9 +74,27 @@ angular.module("dgt-centaur-mods", ['ngMaterial', 'angular-storage', 'ngAnimate'
 				] 
 			}, {
 				label:"System", items: [
-					{ label: "Power off board", action:() => me.socket.emit('request', {'sys_action':'shutdown'}) },
-					{ label: "Reboot board", action:() => me.socket.emit('request', {'sys_action':'reboot'}) },
-					{ label: "Restart service", action:() => me.socket.emit('request', {'sys_action':'restart_service'}) },
+					{ label: "Power off board", action:() => {
+							me.socket.emit('request', {'sys_action':'shutdown'})
+							popupMessage('A shutdown request has been sent to the board!')
+
+						} 
+					},
+					{ label: "Reboot board", action:() => {
+							me.socket.emit('request', {'sys_action':'reboot'})
+							popupMessage('A reboot request has been sent to the board!') 
+						}
+					},
+					{ label: "Restart service", action:() => {
+							me.socket.emit('request', {'sys_action':'restart_service'})
+							popupMessage('A service restart request has been sent to the board!') 
+						}
+					},
+					{ type: "divider"},
+					{ label: "Last log events", action:() => {
+						me.socket.emit('request', {'sys_action':'log_events'})
+					}
+				},
 				]
 			}
 		]
@@ -207,102 +174,205 @@ angular.module("dgt-centaur-mods", ['ngMaterial', 'angular-storage', 'ngAnimate'
 			return board
 		}
 
+		// Stockfish evaluation feedback
+		stockfish.onmessage = (event) => {
+
+			if (event.data) {
+
+				//console.log(event.data)
+
+				// Stockfish evaluation finishes with the bestmove message
+				if (event.data.includes("bestmove")) {
+					me.board.eval = 50+stockfish._lasteval
+					$scope.$apply()
+				}
+				
+				// Stockfish evaluation feedback
+				if (event.data.includes("score cp")) {
+
+					// info depth 1 seldepth 1 multipv 1 score cp -537 nodes
+					const regexp = /score\ cp\ ([^ ]+)\ /gi
+					const matches = regexp.exec(event.data)
+
+					const MAX_VALUE = 600
+
+					if (matches && matches.length) {
+
+						let value = parseInt(matches[1])
+
+						// black plays?
+						if (me.board.turn == 0) value = -value
+			
+						if (value >  MAX_VALUE) value =  MAX_VALUE
+						if (value < -MAX_VALUE) value = -MAX_VALUE
+
+						value = parseInt((value/(MAX_VALUE *2)) * 100)
+
+						stockfish._lasteval = value
+					}
+				}
+
+				// Stockfish detected (future) mat state
+				if (event.data.includes("score mate")) {
+
+					let value = 50
+
+					// black plays?
+					if (me.board.turn == 0) value = -value
+
+					stockfish._lasteval = value
+
+					me.board.eval = 50+value
+					$scope.$apply()
+				}
+			}
+		}
+
+		 var popupMessage = (message) => {
+			$mdDialog.show(
+				$mdDialog.alert()
+				  .clickOutsideToClose(true)
+				  .title('♔ '+document.title+' ♔')
+				  .textContent(message)
+				  .ariaLabel(message)
+				  .ok('Got it!'))
+		 }
+
 		$timeout(() => {
 
 			me.chessboard = buildChessboard(me.board, { keyboard:true })
 
-			me.socket = io();
+			me.socket = io()
+
+			me.socket.on("error", (error) => {
+				console.log(error)
+			})
+
+			me.socket.on("connect", () => {
+				console.log("Connected to the server.")
+			})
+
+			me.socket.on("disconnect", (reason) => {
+				console.log("Disonnected to the server.")
+				if (reason === "io server disconnect") {
+				  // The disconnection was initiated by the server, you need to reconnect manually
+				  me.socket.connect()
+				}
+				// Else the socket will automatically try to reconnect
+			})
 
 			// We receive data from the server app
 			me.socket.on('message', function(message) {
 				//console.log(message)
 
-				if (message.clear_board_graphic_moves) {
-					Chessboard.clearGraphicArrow(me.chessboard)
-				}
-				
-				if (message.fen) {
-
-					if (me.current_fen != message.fen) {
-
-						// We determinate from the FEN which color plays
-						me.board.turn = message.fen.includes(' w ') ? 1 : 0
-
-						me.board.turn_caption = "turn → "+(me.board.turn ? 'white' : 'black')
-
-						me.chessboard.position(message.fen)
-						me.current_fen = message.fen
-
-						// We trigger the evaluation on new FEN only
-						if (me.board.live_evaluation) {
-							stockfish.postMessage("position fen " + message.fen)
-							stockfish.postMessage("go depth 12")
-						}
-					}
-				}
-				
-				// The PGN has been formatted by the server
-				if (message.pgn) {
-					me.current_pgn = message.pgn
-
-					let moves = (() => {
-						const c = new Chess()
-						c.load_pgn(message.pgn)
-						return c.history()
-					})()
-
-					history.fens = [DEFAULT_POSITION]
-
-					var c = new Chess()
-
-					moves.forEach(m => {
-						c.move(m)
-						history.fens.push(c.fen())
-					})
-
-					history.index = history.fens.length-1
-				}
-
 				// Shortcuts to make the code more readable...
 				const arrow = Chessboard.drawGraphicArrow
 				const square = Chessboard.drawGraphicSquare
 
-				// Last move has been taken back - we draw it
-				if (message.uci_undo_move) {
-					arrow(me.chessboard, message.uci_undo_move.slice(0, 2), { color:'orange' })
-					arrow(me.chessboard, message.uci_undo_move, { color:'orange' })
-				}
+				const socketmessages = {
 
-				// We draw the previous move
-				if (me.board.previous_move && message.uci_move) {
-					square(me.chessboard, message.uci_move.slice(0, 2), { color:"black" })
-					arrow(me.chessboard, message.uci_move, { color:"black" })
-				}
+					// We receive last log events
+					log_data: (value) => {
+						me.logData = value.join('\n')
+						$mdDialog.show({
+							contentElement: '#log_data',
+							parent: angular.element(document.body),
+							targetEvent: null,
+							clickOutsideToClose: true
+						})
+					},
 
-				// We draw the computer move
-				if (message.computer_uci_move) {
-					arrow(me.chessboard, message.computer_uci_move, { color:"yellow" })
-				}
-
-				// We draw the hint
-				if (message.tip_uci_move) {
-					arrow(me.chessboard, message.tip_uci_move, { color:"green" })
-				}
-
-				// We draw the checks
-				if (me.board.kings_checks && message.checkers && message.checkers.length>0 && message.kings) {
+					// We clear the graphics (not the user ones)
+					clear_board_graphic_moves: () => Chessboard.clearGraphicArrow(me.chessboard),
 					
-					let kingSquare = message.kings[1 -me.board.turn]
-					square(me.chessboard, kingSquare, { color:"red" })
+					// We receive a new postion
+					fen: (value) => {
+
+						if (me.current_fen != value) {
+	
+							// We determinate from the FEN which color plays
+							me.board.turn = value.includes(' w ') ? 1 : 0
+	
+							me.board.turn_caption = "turn → "+(me.board.turn ? 'white' : 'black')
+	
+							me.chessboard.position(value)
+							me.current_fen = value
+	
+							// We trigger the evaluation on new FEN only
+							if (me.board.live_evaluation) {
+								stockfish.postMessage("position fen " + value)
+								stockfish.postMessage("go depth 12")
+							}
+						}
+					},
+
+					// We receive the current PGN, formatted by the server
+					pgn: (value) => {
+						me.current_pgn = value
+
+						let moves = (() => {
+							const c = new Chess()
+							c.load_pgn(value)
+							return c.history()
+						})()
+
+						// We build the FEN history
+						history.fens = [DEFAULT_POSITION]
+
+						var c = new Chess()
+
+						moves.forEach(m => {
+							c.move(m)
+							history.fens.push(c.fen())
+						})
+
+						history.index = history.fens.length-1
+					},
+
+					// Last move has been taken back - we draw it
+					uci_undo_move: (value) => {
+						arrow(me.chessboard, value.slice(0, 2), { color:'orange' })
+						arrow(me.chessboard, value, { color:'orange' })
+					},
+
+					// We draw the previous move
+					uci_move: (value) => {
 					
-					message.checkers.forEach(item => {	
-						arrow(me.chessboard, item+kingSquare, { color:"red" })
-					})
+						if (me.board.previous_move) {
+							square(me.chessboard, value.slice(0, 2), { color:"black" })
+							arrow(me.chessboard, value, { color:"black" })
+						}
+					},
+
+					// We draw the computer move
+					computer_uci_move: (value) => arrow(me.chessboard, value, { color:"yellow" }),
+
+					// We draw the hint
+					tip_uci_move: (value) => arrow(me.chessboard, value, { color:"green" }),
+				
+					// We draw the checks
+					checkers: (value) => {
+						if (me.board.kings_checks && value.length>0 && message.kings) {
+					
+							let kingSquare = message.kings[1 -me.board.turn]
+							square(me.chessboard, kingSquare, { color:"red" })
+							
+							value.forEach(item => {	
+								arrow(me.chessboard, item+kingSquare, { color:"red" })
+							})
+						}
+					},
+
+					// We override the default turn caption
+					turn_caption: (value) => {
+						me.board.turn_caption = value
+					}
 				}
 
-				// We override the default turn caption
-				if (message.turn_caption) {
-					me.board.turn_caption = message.turn_caption
+				for(var id in socketmessages) {
+					// Does the message contain the id?
+					// If yes we call the function
+					if (message[id]) socketmessages[id](message[id])
 				}
 
 				$scope.$apply()
@@ -310,6 +380,8 @@ angular.module("dgt-centaur-mods", ['ngMaterial', 'angular-storage', 'ngAnimate'
 
 			// We ask the app to send us the current PGN, FEN and previous move
 			me.socket.emit('request', {'fen':true, 'pgn': true, 'uci_move': true})
+
+			popupMessage('Welcome!This web interface is being tested!\nYou can come back to the legacy interface anytime using the menu.')
 
 		}, 500)
 	}
