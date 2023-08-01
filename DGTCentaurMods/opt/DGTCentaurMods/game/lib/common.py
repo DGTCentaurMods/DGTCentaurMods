116a9df1ec92f4ba840933e6424e6fa918aa25e3
Alistair Crompton <alistair.crompton@moult.org>
2023-08-01 12:10:38 +0200

    Moved tail function to common.

--- DGTCentaurMods/opt/DGTCentaurMods/game/lib/common.py
+++ DGTCentaurMods/opt/DGTCentaurMods/game/lib/common.py
@@ -21,7 +21,39 @@
 
 from DGTCentaurMods.game.consts import consts, Enums
 
-import configparser
+import os, configparser
+
+def tail(f, lines=1, _buffer=4098):
+    """Tail a file and get X lines from the end"""
+    # place holder for the lines found
+    lines_found = []
+
+    # block counter will be multiplied by buffer
+    # to get the block size from the end
+    block_counter = -1
+
+    # loop until we find X lines
+    while len(lines_found) < lines:
+        try:
+            f.seek(block_counter * _buffer, os.SEEK_END)
+        except IOError:  # either file is too small, or too many lines requested
+            f.seek(0)
+            lines_found = f.readlines()
+            break
+
+        lines_found = f.readlines()
+
+        # we found enough lines, get out
+        # Removed this line because it was redundant the while will catch
+        # it, I left it for history
+        # if len(lines_found) > lines:
+        #    break
+
+        # decrement the block counter to get the
+        # next X bytes
+        block_counter -= 1
+
+    return lines_found[-lines:]
 
 def get_Centaur_FEN():
 
--- DGTCentaurMods/opt/DGTCentaurMods/web/app.py
+++ DGTCentaurMods/opt/DGTCentaurMods/web/app.py
@@ -28,40 +28,6 @@ from DGTCentaurMods.game.classes import DAL
 
 import os, time, logging
 
-
-def tail(f, lines=1, _buffer=4098):
-    """Tail a file and get X lines from the end"""
-    # place holder for the lines found
-    lines_found = []
-
-    # block counter will be multiplied by buffer
-    # to get the block size from the end
-    block_counter = -1
-
-    # loop until we find X lines
-    while len(lines_found) < lines:
-        try:
-            f.seek(block_counter * _buffer, os.SEEK_END)
-        except IOError:  # either file is too small, or too many lines requested
-            f.seek(0)
-            lines_found = f.readlines()
-            break
-
-        lines_found = f.readlines()
-
-        # we found enough lines, get out
-        # Removed this line because it was redundant the while will catch
-        # it, I left it for history
-        # if len(lines_found) > lines:
-        #    break
-
-        # decrement the block counter to get the
-        # next X bytes
-        block_counter -= 1
-
-    return lines_found[-lines:]
-
-
 logging.getLogger('chess.engine').setLevel(logging.CRITICAL)
 logging.getLogger("requests").setLevel(logging.CRITICAL)
 logging.getLogger("urllib3").setLevel(logging.CRITICAL)
@@ -126,7 +92,7 @@ def on_request(message):
 			os.system("sudo systemctl restart DGTCentaurMods.service")
 
 		if action == "log_events":
-			response["log_events"] = tail(open(consts.LOG_FILENAME, "r"), 100)
+			response["log_events"] = common.tail(open(consts.LOG_FILENAME, "r"), 100)
 			socketio.emit('message', response)
 
 	else:
