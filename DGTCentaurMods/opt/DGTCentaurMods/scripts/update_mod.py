7d5bf26fa265772d40d9e3cb5b3f844cf807a5fe
Alistair Crompton <alistair.crompton@moult.org>
2023-09-09 11:47:07 +0200

    Updated update script.

--- DGTCentaurMods/opt/DGTCentaurMods/scripts/update_mod.py
+++ DGTCentaurMods/opt/DGTCentaurMods/scripts/update_mod.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3 
-import requests
-import subprocess
+import requests, os
 
-print("Cleaning old deb files")
-subprocess.run(["rm", "*.deb"])
+from DGTCentaurMods.consts import consts
+from DGTCentaurMods.classes import Log, SocketClient
 
-print("Uninstalling current version")
-subprocess.run(["sudo", "apt", "-y", "remove", "dgtcentaurmods"])
+SOCKET = SocketClient.get()
 
-latest_release_url = ''
+latest_release_url = ""
 
-print("Checking for the latest release")
-response = requests.get("https://api.github.com/repos/Alistair-Crompton/DGTCentaurMods/releases/latest")
+Log.info("Starting mod update...")
+Log.info("Checking for the latest release...")
+
+SOCKET.send_message({"popup":"Starting mod update!", "standby":True})
+
+response = requests.get(consts.GITHUB_URI)
 
 # if file was downloaded
 if response.status_code == 200:
     response_json = response.json()
     latest_release_url = response_json['assets'][-1]['browser_download_url']
-    print(f"Latest release found: {latest_release_url}")
+    Log.info(f"Latest release found: {latest_release_url}")
 else:
-    print(f"Internet down? Status code: {response.status_code}")
+    Log.exception(f"Internet down? Status code: {response.status_code}")
     exit(-1)
 
 filename = latest_release_url.split('/')[-1]
 
-print(f"Downloading {latest_release_url}")
-response = requests.get(latest_release_url)
+Log.info("Cleaning previous deb files...")
+os.system("sudo rm -f /home/pi/*.deb >/dev/null 2>&1")
 
-# if file was downloaded
-if response.status_code == 200:
-    with open(f"{filename}", "wb") as file:
-        file.write(response.content)
-    print(f"{filename} saved")
-else:
-    print(f"Failed to download. Status code: {response.status_code}")
+Log.info(f"Downloading '{latest_release_url}'...")
+os.system(f"wget {latest_release_url}")
 
+Log.info("DPKG configuration..")
+os.system("sudo sudo dpkg --configure -a")
 
-print("Installing package")
-subprocess.run(["sudo", "apt", "--assume-yes", "install", f"./{filename}"])
+SOCKET.send_message({"popup":"Installing new package!"})
 
-print("Rebooting!")
-subprocess.run(["sudo", "reboot", "now"])
+Log.info("Uninstalling current version...")
+os.system("sudo apt remove -y dgtcentaurmods")
+
+Log.info("Installing package...")
+os.system(f"sudo apt install -y ./{filename}")
+
+Log.info("Rebooting!")
+os.system("sudo reboot")
