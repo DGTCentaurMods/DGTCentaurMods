a7e1724f289f01e97a453c488ce5746fb887522e
Alistair Crompton <alistair.crompton@moult.org>
2023-09-12 16:19:36 +0200

    Updated version.

--- DGTCentaurMods/opt/DGTCentaurMods/consts/consts.py
+++ DGTCentaurMods/opt/DGTCentaurMods/consts/consts.py
@@ -25,7 +25,7 @@ from pathlib import Path
 
 MAIN_ID = "DGTCentaurMods"
 
-TAG_RELEASE = "ON23091102"
+TAG_RELEASE = "ON23091103"
 
 GITHUB_URI = "https://api.github.com/repos/Alistair-Crompton/DGTCentaurMods/releases/latest"
 
