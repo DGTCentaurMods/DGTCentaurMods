3ddef3a05e0704ef7dd42434f401977ed8288eae
Alistair Crompton <alistair.crompton@moult.org>
2023-09-15 20:51:41 +0200

    Added comments to Squiz plugin.

--- DGTCentaurMods/opt/DGTCentaurMods/plugins/Squiz.py
+++ DGTCentaurMods/opt/DGTCentaurMods/plugins/Squiz.py
@@ -26,18 +26,25 @@ import chess, random
 
 QUESTIONS_COUNT:int = 20
 
+# The plugin must inherits of the Plugin class.
+# Filename must match the class name.
 class Squiz(Plugin):
 
+    # Constructor for initialization stuff
+    # The id is the name of the plugin/class
     def __init__(self, id:str):
         super().__init__(id)
 
-        self.intialize()
+        self.initialize()
 
-    def intialize(self):
+    # Initialization function,
+    # invoked on each new game.
+    def initialize(self):
         self._started:bool = False
         self._qindex:int = 0
         self._bonus:int = QUESTIONS_COUNT*3
 
+    # Game ends.
     def game_over(self):
 
         Centaur.clear_screen()
@@ -54,18 +61,24 @@ class Squiz(Plugin):
         Centaur.print("Press PLAY", row=11)
         Centaur.print("To retry!")
 
-        self.intialize()
+        self.initialize()
 
+    # Generate a new question,
+    # choosing a random square then displaying it.
     def generate_question(self):
 
+        # Next question...
         self._qindex += 1
 
+        # Do we reach the limit?
         if self._qindex == QUESTIONS_COUNT+1:
             self.game_over()
             return
 
         Centaur.clear_screen()
 
+        # We create a random square.
+        # The square_name function needs a number from 0 to 63.
         self._random_square = chess.square_name(random.randint(0,63))
 
         Centaur.print("Question", row=2)
@@ -76,6 +89,8 @@ class Squiz(Plugin):
         Centaur.print()
         Centaur.print(self._random_square, font=fonts.DIGITAL_FONT)
 
+    # This function is automatically invoked when
+    # the user launches the plugin.
     def start(self):
         super().start()
 
@@ -86,9 +101,16 @@ class Squiz(Plugin):
         Centaur.print("start")
         Centaur.print("the game!")
 
+    # This function is (automatically) invoked when
+    # the user stops the plugin
     def stop(self):
+
+        # Back to the main menu.
         super().stop()
 
+    # This function is automatically invoked each
+    # time the player press a key.
+    # Except the BACK key which is handled by the engine.
     def key_callback(self, key:Enums.Btn):
 
         if key == Enums.Btn.PLAY and not self._started:
@@ -98,11 +120,14 @@ class Squiz(Plugin):
             
             self.generate_question()
 
+        # If the user press HELP,
+        # we display the correct square.
         if key == Enums.Btn.HELP and self._started:
             Centaur.sound(Enums.Sound.TAKEBACK_MOVE)
             Centaur.flash(self._random_square)
 
-    
+    # This function is automatically invoked each
+    # time the player moves a piece.
     def field_callback(self,
                 square:str,
                 field_action:Enums.PieceAction,
@@ -111,19 +136,23 @@ class Squiz(Plugin):
         if not self._started:
             return
         
+        # We care only when the user drop a piece.
         if field_action == Enums.PieceAction.PLACE:
 
             Centaur.flash(square)
 
+            # Correct answer?
             if self._random_square == square:
                 Centaur.sound(Enums.Sound.CORRECT_MOVE)
 
+                # Next question...
                 self.generate_question()
 
             else:
                 Centaur.sound(Enums.Sound.WRONG_MOVE)
                 Centaur.print("WRONG!", row=11)
 
+                # Wrong answer, we decrease the bonus.
                 self._bonus -= 1
 
                 if self._bonus == 0:
