0d1134706fec2b941a21f77fc0bd9613b17bf4dc
Alistair Crompton <alistair.crompton@moult.org>
2023-09-27 07:41:19 +0200

    Added node.js server.

---
+++ DGTCentaurMods/node.js/main.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('<h2>Alistair-Centaur-Mods socket.io server</h2>')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+    io.on('connection', (socket) => {
+        socket.on('message', (message) => {
+            io.emit('message', message)
+        })
+    })
+})
+
+server.listen(port, () => {
+    console.log('listening on *:'+port)
+})
