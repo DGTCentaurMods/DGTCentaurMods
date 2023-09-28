933db70990bfc270e135d299f3b2a1cb48fc19b0
Alistair Crompton <alistair.crompton@moult.org>
2023-09-28 10:55:01 +0200

    Added PosgreSQL statements.

--- DGTCentaurMods/node.js/main.js
+++ DGTCentaurMods/node.js/main.js
@@ -6,10 +6,43 @@ const { Server } = require("socket.io")
 const io = new Server(server)
 
 const port = process.env.PORT || 3000
+const connectionString = process.env.PGSTRING
 
-app.get('/', (req, res) => {
-    res.send('<h2>Alistair-Centaur-Mods socket.io server</h2>')
-})
+console.log(process.argv)
+
+console.log("PORT (ENV):"+process.env.PORT)
+console.log("PORT:"+port)
+console.log("PG_CSTRING:"+connectionString)
+
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({ connectionString })
+
+//app.get('/', (req, res) => {
+//    res.send('<h2>Alistair-Centaur-Mods socket.io server</h2>')
+//})
+
+async function main(){
+
+    const client = new Client({ connectionString })
+ 
+    try {
+
+        const result = await pool.query('SELECT NOW()')
+        console.log(result.rows[0])
+
+        //await client.connect()
+        //console.log('Database Connected.')
+
+    } catch (e) {
+        console.error(e)
+    } finally {
+        //await client.end()
+        console.log('Done.')
+    }
+}
+ 
+main().catch(console.error)
 
 io.on('connection', (socket) => {
     console.log('A user is connected.')
